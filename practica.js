ef5edde85f61144818ba7589e65df49a8c8ca01a
Ana <arperezch13@alumnos.cei.es>
2026-02-03 21:59:58 +0100

    Implement searchPost function to fetch and display post title

---
+++ practica.js
@@ -0,0 +1,12 @@
+async function searchPost(id) {
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        const data = await response.json()
+        console.log(data.title.toUpperCase());
+    } 
+    catch (error) {
+        console.log("El post no pudo ser hallado");
+    }
+}
+
+searchPost()
